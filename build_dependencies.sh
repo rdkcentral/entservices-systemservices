8929ef1bc9798e5aa383fa39af7ff2fbe884e39d
smohap466_comcast <srinibas_mohapatra@comcast.com>
2026-08-06 11:44:43 +0530

    RDKEMW-22169: Updating Thunder branch and sha value

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -20,6 +20,8 @@
 set -x
 set -e
 ##############################
+THUNDER_TOOLS_COMMIT_SHA="d5dd83c7c19c49c7f25c558c126500bd2d64f7a4"
+THUNDER_COMMIT_SHA="2c0fcc5529e7da734be558ca6efa05d934dcce31"
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}
@@ -34,9 +36,15 @@ pip install jsonref
 # Clone the required repositories
 
 
-git clone --branch  R4.4.6 https://github.com/rdkcentral/ThunderTools.git
+git clone -b R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
+cd ThunderTools
+git checkout $THUNDER_TOOLS_COMMIT_SHA
+cd ..
 
-git clone --branch R4.4.6 https://github.com/rdkcentral/Thunder.git
+git clone -b R4_4-RDK https://github.com/rdkcentral/Thunder.git
+cd Thunder
+git checkout $THUNDER_COMMIT_SHA
+cd ..
 
 git clone --branch RDKEMW-19266 https://github.com/rdkcentral/entservices-apis.git
 
@@ -51,7 +59,6 @@ git clone --branch feature/RDKEMW-22169 https://github.com/rdkcentral/entservice
 echo "======================================================================================"
 echo "buliding thunderTools"
 cd ThunderTools
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4.6-Add-support-for-project-dir.patch
 cd -
 
 
@@ -70,9 +77,6 @@ echo "==========================================================================
 echo "buliding thunder"
 
 cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4_6.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-R4.4.6-Add-support-for-project-dir.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Jsonrpc_dynamic_error_handling_R4.4.6.patch
 cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
