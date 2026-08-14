8e5049d673a3889a04064c90180af2425e8af1b0
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2026-08-14 10:14:28 -0400

    RDKEMW-23342 : Implement Blockflag to use MFR functions

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -77,7 +77,7 @@ echo "==========================================================================
 echo "buliding thunder"
 
 cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4_4-RDK.patch
 patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
 patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
 patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
