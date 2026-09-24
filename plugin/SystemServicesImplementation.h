8fc7d782e4b4fd404cbe9f085b20bf57e1a74804
rkiran696 <Raja_Kiran@comcast.com>
2026-09-24 11:30:17 +0530

    Cherrypicked sucesss files function signature

--- plugin/SystemServicesImplementation.h
+++ plugin/SystemServicesImplementation.h
@@ -336,7 +336,7 @@ namespace WPEFramework
             void Dispatch(Event event, const JsonObject &params);
 
             static void getMacAddressesAsync(SystemServicesImplementation *p);
-            static int startModeTimer(int duration);
+            static float startModeTimer(int duration);
             static void stopModeTimer();
             static void updateDuration();
             bool processTimeZones(std::string entry, JsonObject& out);
